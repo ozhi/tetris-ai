e880e201f68f400cae26c58a004e146d638aac6c
Bozhin Katsarski <bozhin@gmail.com>
2019-01-14 12:15:59 +0200

    Add docs for type board

--- internal/tetris/board.go
+++ internal/tetris/board.go
@@ -4,22 +4,31 @@ import (
 	"fmt"
 )
 
+// The default tetris board size.
 const (
 	defaultBoardWidth  = 10
 	defaultBoardHeight = 20
 )
 
+// tetrominoMatrices is the slice of matrices for each rotation of each tetromino.
+// tetrominoMatrices is a global variable because it is shared between all boards.
+// TODO: Is there a more elegant way to do this?
 var tetrominoMatrices [][]TetrominoMatrix
 
 func init() {
-	// Is this a good way to initialize matrices?
 	tetrominoMatrices = TetrominoMatrices()
 }
 
+// Board is a tetris Board.
+// Board allows dropping tetrominoes and keeps statistics about the game so far and the current state.
+// The zero value of Board is not usable, a new board must be created with the NewBoard method.
 type Board struct {
 	width  int
 	height int
-	cells  [][]Tetromino
+
+	// cells contains the cells of the board.
+	// Cells are indexed from 0 from left to right and top to bottom.
+	cells [][]Tetromino
 
 	gameOver bool
 
@@ -29,6 +38,7 @@ type Board struct {
 	holes              []int
 }
 
+// NewBoard returns a pointer to a new Board.
 func NewBoard() *Board {
 	board := Board{
 		width:         defaultBoardWidth,
@@ -45,6 +55,7 @@ func NewBoard() *Board {
 	return &board
 }
 
+// NewBoardFromBoard returns a pointer to an independent copy of the given board.
 func NewBoardFromBoard(other *Board) *Board {
 	board := *other
 
@@ -63,22 +74,27 @@ func NewBoardFromBoard(other *Board) *Board {
 	return &board
 }
 
+// Width returns the width of the board.
 func (b *Board) Width() int {
 	return b.width
 }
 
+// Height returns the height of the board.
 func (b *Board) Height() int {
 	return b.height
 }
 
+// GameOver returns true if the game of the current board is over.
 func (b *Board) GameOver() bool {
 	return b.gameOver
 }
 
+// ClearedLines returns the number of lines cleared on the given board.
 func (b *Board) ClearedLines() int {
 	return b.clearedLines
 }
 
+// DroppedTetrominoes returns the number of tetrominoes that have been dropped on the given board.
 func (b *Board) DroppedTetrominoes() int {
 	return b.droppedTetrominoes
 }
@@ -91,6 +107,10 @@ func (b *Board) ColumnHoles() []int {
 	return b.holes
 }
 
+// At returns the tetromino at the given position of the board.
+// If the given position is not occupied, TetrominoEmpty is returned.
+// The cells of the board are indexed from 0, left to right and top to bottom.
+// At panics if invalid coordinates are provided.
 func (b *Board) At(row, col int) Tetromino {
 	if !b.isValidCell(row, col) {
 		panic(fmt.Errorf("Board.At: invalid coordinates (%d, %d) provided", row, col))
@@ -99,6 +119,8 @@ func (b *Board) At(row, col int) Tetromino {
 	return b.cells[row][col]
 }
 
+// Drop drops a specified rotation of a tetromino such that the leftmost cell is in the given column.
+// Drop panics if the given tetromino, rotation or column are invalid.
 func (b *Board) Drop(tetromino Tetromino, rotation int, column int) error {
 	if !tetromino.Valid() {
 		panic(fmt.Errorf("Board.Drop: invalid tetromino %d provided", tetromino))
@@ -118,7 +140,7 @@ func (b *Board) Drop(tetromino Tetromino, rotation int, column int) error {
 
 	tetrominoMatrix := tetrominoMatrices[tetromino][rotation]
 
-	if !b.isValidColumn(column, tetrominoMatrix) {
+	if column+len(tetrominoMatrix[0]) > b.width {
 		panic(fmt.Errorf(
 			"Board.Drop: can not drop: invalid column %d provided for tetromino %s, rotation %d",
 			column, tetromino, rotation,
@@ -187,6 +209,9 @@ func (b *Board) Drop(tetromino Tetromino, rotation int, column int) error {
 	return nil
 }
 
+// canBePut returns true if the given tetromino matrix's can be put on the board
+// with its top left cell at coordinates row, col.
+// If the matrix sticks out of the board or overlaps a non-empty cell on the board, false is returned.
 func (b *Board) canBePut(tetrominoMatrix TetrominoMatrix, row, col int) bool {
 	for i := range tetrominoMatrix {
 		for j := range tetrominoMatrix[i] {
@@ -200,11 +225,14 @@ func (b *Board) canBePut(tetrominoMatrix TetrominoMatrix, row, col int) bool {
 	return true
 }
 
+// isValidCell returns true if the given coordinates are valid for the board.
 func (b *Board) isValidCell(row, col int) bool {
 	return 0 <= row && row < b.height &&
 		0 <= col && col < b.width
 }
 
+// isValidCell returns true if the given coordinates are valid for the board.
+// isFullRow returns true if all of the cells in the given row of the board are non-empty.
 func (b *Board) isFullRow(row int) bool {
 	for _, cell := range b.cells[row] {
 		if cell == TetrominoEmpty {
@@ -214,6 +242,9 @@ func (b *Board) isFullRow(row int) bool {
 	return true
 }
 
+// clearFullRows traverses the board and clears any full rows.
+// The rows above are then shifted down and the board is filled with emoty rows at the top.
+// clearFullRows returns true if at least one row was cleared.
 func (b *Board) clearFullRows() bool {
 	var (
 		someRowsCleared = false
@@ -243,21 +274,4 @@ func (b *Board) clearFullRows() bool {
 	}
 
 	return someRowsCleared
-}
-
-func (b *Board) isValidColumn(column int, tetrominoMatrix TetrominoMatrix) bool {
-	if column < 0 {
-		return false
-	}
-
-	tetrominoWidth := 0
-	for row := range tetrominoMatrix {
-		for col := range tetrominoMatrix[row] {
-			if tetrominoMatrix[row][col] && col > tetrominoWidth {
-				tetrominoWidth = col
-			}
-		}
-	}
-
-	return column+tetrominoWidth < b.width
 }
